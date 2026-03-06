a2b8f4a487ce1b19f7252817e1c64099e46a5f66
Aarmbh <aarmbh15@gmail.com>
2026-03-06 12:21:16 +0530

    shweta

--- src/components/Pages/OurWork/OurWork.jsx
+++ src/components/Pages/OurWork/OurWork.jsx
@@ -1,122 +1,165 @@
 import React, { useEffect, useState } from "react";
 import style from "./OurWork.module.css";
-import OurWorkCard from './OurWorkCard/OurWorkCard';
+import OurWorkCard from "./OurWorkCard/OurWorkCard";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-// Video Imports
-import One8 from '../../../video/One8.mp4';
-import VIPChai from '../../../video/Payment At Tapri_VIPS Wallet.mp4';
-import Actimax from '../../../video/ACTIMAX - BRAND MASTER.mp4';
-import DevFest from '../../../video/Devfest 2022 with KNM studio.mp4';
-import LipBalm from '../../../video/Lip Balm Female.mp4';
-import MCEA from '../../../video/MCEA Event Teaser.mp4';
+// VIDEO IMPORTS
+import One8 from "../../../video/One8.mp4";
+import VIPChai from "../../../video/Payment At Tapri_VIPS Wallet.mp4";
+import Actimax from "../../../video/ACTIMAX - BRAND MASTER.mp4";
+import DevFest from "../../../video/Devfest 2022 with KNM studio.mp4";
+import LipBalm from "../../../video/Lip Balm Female.mp4";
+import MCEA from "../../../video/MCEA Event Teaser.mp4";
 
-// Thumbnail Imports
-import ABM from '../../../img/OurWorkThumnail/Kensha.jpg';
-import Greenfront from '../../../img/OurWorkThumnail/Greenfront.jpg';
-// import ABM from '../../../img/OurWorkThumnail/Kensha.jpg';
-// import ABM from '../../../img/OurWorkThumnail/Kensha.jpg';
-// import ABM from '../../../img/OurWorkThumnail/Kensha.jpg';
-// import ABM from '../../../img/OurWorkThumnail/Kensha.jpg';
-// ... (Keep all your other thumbnail imports here)
+// IMAGE IMPORTS
+import ARaymondCSR from "../../../img/OurWorkThumnail/ARaymond csr.jpg";
+import ARaymondSolar from "../../../img/OurWorkThumnail/ARaymonds solar plant.jpg";
+import Dryfruits from "../../../img/OurWorkThumnail/Dryfruits.jpg";
+import GodrejTypography from "../../../img/OurWorkThumnail/Godrej green front success Typography.jpg";
+import GodrejGreens from "../../../img/OurWorkThumnail/Godrej Greens.jpg";
+import GodrejQuality from "../../../img/OurWorkThumnail/Godrej Quality av.jpg";
+import OPmobility from "../../../img/OurWorkThumnail/OPmobility (Manufacturing).jpg";
+import Padcare from "../../../img/OurWorkThumnail/Padcare knm strategy.jpg";
+import SukoonX300 from "../../../img/OurWorkThumnail/Sukoon x300.jpg";
+import VivoV60 from "../../../img/OurWorkThumnail/Vivo V60.jpg";
+import AraymondPlantII from "../../../img/OurWorkThumnail/A raymond Plant II.jpg";
+import ElarisSuccessvideo from "../../../img/OurWorkThumnail/Elaris Success video.jpg";
+
 
 const OurWork = () => {
-    const [displayType, setDisplayType] = useState("ALL");
 
-    useEffect(() => {
-        AOS.init({ duration: 1000 });
-    }, []);
+  const [displayType,setDisplayType] = useState("ALL");
 
-    const sliderSettings = {
-        dots: false,
-        infinite: true,
-        speed: 2000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 7000,
-        cssEase: "linear",
-        arrows: false
-    };
+  useEffect(()=>{
+    AOS.init({duration:1000});
+  },[]);
 
-    const VideoData = [
-        { key: 1, src: One8, title: "ONE 8 - ATTITUDE", subTitle: "⦿ Advertisement", discription: "Embracing the bold attitude that comes after wearing the undergarments of One8." },
-        { key: 2, src: MCEA, title: "MCA Award Night", subTitle: "⦿ Event Documentation", discription: "In recognition of their untapped potential, MCA decided to honor the ground and office staff." },
-        { key: 3, src: VIPChai, title: "VIPS Wallet", subTitle: "⦿ TVC - Advertisement", discription: "Vips wallet provides a seamless platform for UPI transactions." },
-        { key: 4, src: Actimax, title: "ACTIMAX", subTitle: "⦿ E - Commerce", discription: "Actimax combines comfort and style, making it unstoppable in performance." },
-        { key: 5, src: LipBalm, title: "AEGTE - LIP", subTitle: "⦿ Advertisement", discription: "Aegte's organic beetroot lip and cheek tint balm." },
-        { key: 6, src: DevFest, title: "GDG - DEV FEST", subTitle: "⦿ Event", discription: "Meet developers and technologists from various industries." }
-    ];
+  const sliderSettings = {
+    dots:false,
+    infinite:true,
+    speed:800,
+    slidesToShow:1,
+    slidesToScroll:1,
+    autoplay:true,
+    autoplaySpeed:7000,
+    arrows:false,
+    pauseOnHover:false
+  };
 
-    const OurWorkDATAOne = [
-        { src: ABM, Vtype: "ECommerce", title: "Vivo X300" },
-        { src: ABM, Vtype: "ECommerce", title: "Vivo V60" },
-        { src: ABM, Vtype: "ECommerce", title: "OPmobilty" },
-        { src: ABM, Vtype: "ECommerce", title: "ARaymond" },
-        { src: ABM, Vtype: "ECommerce", title: "Padcare" },
-        { src: ABM, Vtype: "ECommerce", title: "Godrej Grenns" },
-        { src: ABM, Vtype: "ECommerce", title: "Godrej Quality wheel" },
-        { src: ABM, Vtype: "ECommerce", title: "Dryfruits" },
-        { src: ABM, Vtype: "ECommerce", title: "Kensha" },
-        { src: Greenfront, Vtype: "ECommerce", title: "Godrej Typography" }
-    ];
+  const VideoData = [
+    {key:1,src:One8,title:"ONE 8 - ATTITUDE",subTitle:"⦿ Advertisement"},
+    {key:2,src:MCEA,title:"MCA Award Night",subTitle:"⦿ Event Documentation"},
+    {key:3,src:VIPChai,title:"VIPS Wallet",subTitle:"⦿ TVC - Advertisement"},
+    {key:4,src:Actimax,title:"ACTIMAX",subTitle:"⦿ E-Commerce"},
+    {key:5,src:LipBalm,title:"AEGTE - LIP",subTitle:"⦿ Advertisement"},
+    {key:6,src:DevFest,title:"GDG - DEV FEST",subTitle:"⦿ Event"}
+  ];
 
-    return (
-        <div className={style.mainContainer}>
-            {/* Integrated Video Slider Header */}
-            <div className={style.heroSliderSection}>
-                <Slider {...sliderSettings}>
-                    {VideoData.map((video) => (
-                        <div key={video.key} className={style.slideWrapper}>
-                            <video src={video.src} autoPlay muted loop className={style.bgVideo} />
-                            <div className={style.videoOverlay}>
-                                <div className={style.headerTextContent} data-aos="fade-up">
-                                    <span className={style.workTag}>OUR WORK</span>
-                                    <h1 className={style.mainVideoTitle}>{video.title}</h1>
-                                    <p className={style.videoSub}>{video.subTitle}</p>
-                                    <p className={style.videoDesc}>{video.discription}</p>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </Slider>
-            </div>
+  const OurWorkDATAOne = [
+    {src:SukoonX300,Vtype:"ECommerce",title:"Sukoon X300"},
+    {src:VivoV60,Vtype:"ECommerce",title:"Vivo V60"},
+    {src:OPmobility,Vtype:"ECommerce",title:"OPMobility Manufacturing"},
+    {src:ARaymondCSR,Vtype:"ECommerce",title:"ARaymond CSR"},
+    {src:ARaymondSolar,Vtype:"ECommerce",title:"ARaymond Solar Plant"},
+    {src:Padcare,Vtype:"ECommerce",title:"Padcare Strategy"},
+    {src:GodrejGreens,Vtype:"ECommerce",title:"Godrej Greens"},
+    {src:GodrejQuality,Vtype:"ECommerce",title:"Godrej Quality AV"},
+    {src:GodrejTypography,Vtype:"ECommerce",title:"Godrej Typography"},
+    {src:Dryfruits,Vtype:"ECommerce",title:"Dry Fruits Campaign"},
+     {src:AraymondPlantII,Vtype:"ECommerce",title:"A raymond Plant II"},
+      {src:ElarisSuccessvideo,Vtype:"ECommerce",title:"Elaris Success video"}
+  ];
 
-            {/* Gallery Section */}
-            <div className={style.subContaierSecond} data-aos="fade-up">
-                <div className={style.subTitle}>SOME OF OUR BEST WORKS</div>
-                
-                <div className={style.subHead}>
-                    {["Advertisement", "Events", "Corporate_Videos", "Informative Videos", "ECommerce"].map((type) => (
-                        <div 
-                            key={type}
-                            className={displayType === type ? style.subHeadActionACTIVE : style.subHeadAction}
-                            onClick={() => setDisplayType(displayType === type ? "ALL" : type)}
-                        >
-                            {type.replace('_', ' ')}
-                        </div>
-                    ))}
+  return(
+    <div className={style.mainContainer}>
+
+      {/* HERO VIDEO SLIDER */}
+
+      <div className={style.heroSliderSection}>
+        <Slider {...sliderSettings}>
+
+          {VideoData.map((video)=>(
+            <div key={video.key} className={style.slideWrapper}>
+
+              <video
+                className={style.bgVideo}
+                autoPlay
+                muted
+                loop
+                playsInline
+              >
+                <source src={video.src} type="video/mp4"/>
+              </video>
+
+              <div className={style.videoOverlay}>
+                <div className={style.headerTextContent} data-aos="fade-up">
+
+                  <span className={style.workTag}>OUR WORK</span>
+
+                  <h1 className={style.mainVideoTitle}>
+                    {video.title}
+                  </h1>
+
+                  <p className={style.videoSub}>
+                    {video.subTitle}
+                  </p>
+
                 </div>
+              </div>
 
-                <div className={style.Dis}>
-                    {OurWorkDATAOne.map((el, index) => (
-                        <OurWorkCard 
-                            key={index}
-                            src={el.src}
-                            title={el.title}
-                            Vtype={el.Vtype}
-                            displayType={displayType}
-                        />
-                    ))}
-                </div>
             </div>
+          ))}
+
+        </Slider>
+      </div>
+
+
+      {/* GALLERY */}
+
+      <div className={style.subContaierSecond}>
+
+        <div className={style.subTitle}>
+          SOME OF OUR BEST WORKS
         </div>
-    );
-};
+
+        <div className={style.subHead}>
+
+          {["Advertisement","Events","Corporate_Videos","Informative Videos","ECommerce"].map((type)=>(
+            <div
+              key={type}
+              className={displayType===type?style.subHeadActionACTIVE:style.subHeadAction}
+              onClick={()=>setDisplayType(displayType===type?"ALL":type)}
+            >
+              {type.replace("_"," ")}
+            </div>
+          ))}
+
+        </div>
+
+        <div className={style.Dis}>
+
+          {OurWorkDATAOne.map((el,index)=>(
+            <OurWorkCard
+              key={index}
+              src={el.src}
+              title={el.title}
+              Vtype={el.Vtype}
+              displayType={displayType}
+            />
+          ))}
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
 
 export default OurWork;
--- src/components/Pages/OurWork/OurWorkCard/OurWorkCard.jsx
+++ src/components/Pages/OurWork/OurWorkCard/OurWorkCard.jsx
@@ -1,34 +1,27 @@
-import style from './OurWorkCard.module.css';
+import style from "./OurWorkCard.module.css";
 
-const OurWorkCard = (props) =>{
+const OurWorkCard = (props) => {
 
-    return (
-        <>
-                {
+  if (props.displayType !== "ALL" && props.displayType !== props.Vtype) {
+    return null;
+  }
 
-                props.displayType === "ALL"?
-                <div className={style.container}>
-                    <div className={style.card}>
-                        <img className={style.cardImg} src={props.src} width={400} />
-                       <div className={style.cardText}> 
-                            <h2 className={style.Vtype} >{props.Vtype}</h2>
-                            <h1 className={style.title}>{props.title}</h1>
-                       </div> 
-                    </div>
-                </div>:
-                props.displayType === props.Vtype ?
-                <div className={style.container}>
-                <div className={style.card}>
-                    <img className={style.cardImg} src={props.src} width={400} />
-                   <div className={style.cardText}> 
-                        <h2 className={style.Vtype} >{props.Vtype}</h2>
-                        <h1 className={style.title}>{props.title}</h1>
-                   </div> 
-                </div>
-            </div> :null
+  return (
+    <div className={style.container}>
+      <div className={style.card}>
+        <img
+          className={style.cardImg}
+          src={props.src}
+          alt={props.title}
+        />
 
-}
-        </>
-    );
-}
+        <div className={style.cardText}>
+          <h2 className={style.Vtype}>{props.Vtype}</h2>
+          <h1 className={style.title}>{props.title}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default OurWorkCard;
